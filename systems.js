a1eb6c785127b0051e4a9510d41cd04014c3ecea
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-09-05 14:12:41 -0400

    Update systems.js

--- systems.js
+++ systems.js
@@ -4,7 +4,6 @@ const system = new URLSearchParams(queryString).get("sys");
 const title = document.getElementById("title");
 
 const sysObject = getSystem();
-renderTitle();
 
 
 
@@ -36,3 +35,38 @@ async function renderTitle() {
     
     title.innerHMTL = `<h1><span style="color: #${colour};">${name}</span> System Info</h1>`
 }
+
+function showInput(reason) {
+    let label;
+
+    if (reason == 404) {
+        // Not found
+        label = "There is no system by that ID."
+    }
+    else if (reason == 403) {
+        // Forbidden
+        label = "This system has their fronters hidden."
+    }
+    else if (reason == null) {
+        // No system ID provided
+        label = "Please enter a system ID:"
+    };
+
+    // Create form for inputting system ID
+    container.innerHTML = `<form>
+                            <label name="sys">${label}</label>
+                            <input type="text" name="sys">
+                            <input type="submit" value="Submit">
+                        </form>`
+}
+
+// Handles which display appears on the page
+if (system != null & system != "") {
+    // Display fronters for requested system
+    container.innerHTML = `<code>Loading fronters...</code>`
+    renderFronters();
+}
+else {
+    // Display system input
+    showInput(null)
+};
