66e1ef8c3cf5747a56ac9e031d531478a3ae873e
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-09-05 13:38:39 -0400

    Update systems.js

--- systems.js
+++ systems.js
@@ -1,1 +1,16 @@
+const queryString = window.location.search;
+const system = new URLSearchParams(queryString).get("sys");
 
+const title = document.getElementById("title");
+
+
+async function getSystem() {
+    let response = await fetch("https://api.pluralkit.me/v2/systems/" + system);
+
+    if (response.status != 200) {
+        showInput(response.status)
+        return null
+    }
+
+    return await response.json()
+}
