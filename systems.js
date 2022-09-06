bbfbec23469bac6ea2c1aba5d21feae0c5fda683
technoalli3 <92867135+technoalli3@users.noreply.github.com>
2022-09-06 16:07:16 -0400

    work on systems page

--- systems.js
+++ systems.js
@@ -4,10 +4,21 @@ const system = new URLSearchParams(queryString).get("sys");
 
 renderTitle();
 renderAttributes();
+renderMembers();
 
 
 async function getSystem() {
     let response = await fetch("https://api.pluralkit.me/v2/systems/" + system);
+
+    if (response.status != 200) {
+        return null
+    }
+
+    return await response.json()
+}
+
+async function getMembers() {
+    let response = await fetch("https://api.pluralkit.me/v2/systems/" + system + "/members");
 
     if (response.status != 200) {
         return null
@@ -32,8 +43,14 @@ async function renderTitle() {
     } else {
         colour = "FFFFFF";
     }
-    
-    let html = `<h1><span style="color: #${colour};">${name}</span> System Info</h1>`;
+
+    let html
+
+    if(sysObject.avatar_url != null) {
+        html = `<h1 style = "background-image:url('${sysObject.avatar_url}');"><span style="color: #${colour};">${name}</span> System Info</h1>`;
+    } else {
+        html = `<h1><span style="color: #${colour};">${name}</span> System Info</h1>`;
+    }
 
     document.getElementById("systemTitle").innerHTML = html;
 }
@@ -42,13 +59,23 @@ async function renderAttributes() {
     const sysObject = await getSystem();
     let html
 
-    if(sysObject.description == null) {
-        html = "No description.";
+    if(sysObject.avatar_url == null) {
+        html = `<img src="blank_system.png" alt="Profile Picture">`
     } else {
-        html = await getDescription(sysObject);
+        html = `<img src="${sysObject.avatar_url}" alt="Profile Picture">`
     }
 
+    html += `<hr style="width:25%; margin-right:75%">`
+    html += `<h2>Description:</h2>`
 
+    if(sysObject.description == null) {
+        html += `<p>No description</p>`
+    } else {
+        html += await getDescription(sysObject);
+    }
+
+    html += `<br><hr style="width:25%; margin-right:75%">`
+    html += `<h2>System Members:</h2>`
 
     document.getElementById("attributes").innerHTML = html;
 }
@@ -57,4 +84,46 @@ async function renderAttributes() {
 
 async function getDescription(sysObject) {
     return marked.parse(sysObject.description);
+}
+
+async function renderMembers() {
+    const members = await getMembers();
+
+    console.log(members)
+    let html
+
+    for(let i = 0; i < members.length; i++) {
+        // Avatar logic
+        let avatar
+
+        if (members[i].avatar_url != null) {
+            // member's avatar
+            avatar = `<img src="${members[i].avatar_url}" alt="Profile Picture", style="float:left;">`
+        }
+        else {
+            // Use placeholder avatar if there is no avatar.
+            avatar = `<img src="blank.png" alt="Profile Picture", style="float:left;">`
+        }
+
+        // Pronouns logic (Alli)
+        let memberPronouns
+
+        if (members[i].pronouns != null) {
+            memberPronouns = members[i].pronouns
+        } else {
+            // Fallback for if member has no pronouns set
+            memberPronouns = "This member has no pronouns set."
+        }
+
+        // Build member item
+        let htmlSegment = `<div class="fronter grid-item">
+                            ${avatar}
+                            <h2>${members[i].name}</h2>
+                            <p>${memberPronouns}</p>
+                        </div>
+                        <br style="clear:both">`;
+
+        html += htmlSegment;
+    }
+    document.getElementById("members").innerHTML = html;
 }
